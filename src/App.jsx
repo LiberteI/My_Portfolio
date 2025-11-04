38b0991aba033065651bb6b84d3da182cc1a45d4
LiberteI <liberteix@gmail.com>
2025-11-04 16:39:28 -0400

    added app border & fixed navbar style

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Navbar from './Components/Navbar/Navbar'
+import Home from './Components/Home/Home'
 
 const App = () => {
   return (
     <div className='app-container'>
       <Navbar/>
+      <Home />
     </div>
   )
 
---
+++ src/Components/Home/Home.jsx
@@ -0,0 +1,11 @@
+import './Home.css'
+
+const Home = () => {
+    return(
+        <main className='home'>
+            
+        </main>
+    )
+}
+
+export default Home
