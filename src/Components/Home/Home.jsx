be1782db001de031ec5f7ff3ef0893323c5708ce
LiberteI <liberteix@gmail.com>
2025-11-05 11:52:17 -0400

    added bg layers

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -2,6 +2,10 @@ import './Home.css'
 
 import moon from '../../assets/City/bg/Moon.png'
 import sky from '../../assets/City/bg/Sky.png'
+import buildingBack from '../../assets/City/bg/buildingBack.png'
+import buildingFar from '../../assets/City/bg/buildingFar.png'
+import buildingMid from '../../assets/City/bg/buildingMid.png'
+import buildingClose from '../../assets/City/bg/buildingClose.png'
 
 const Home = () => {
     return(
@@ -10,9 +14,14 @@ const Home = () => {
                 
                 <img className='home-bg-sky' src={sky} alt="sky" />
                 <img className='home-bg-moon' src={moon} alt="moon" />
+                <img className='home-bg-building-far' src={buildingFar} alt="Far skyline" />
+                <img className='home-bg-building-mid' src={buildingMid} alt="Mid skyline" />
+                <img className='home-bg-building-back' src={buildingBack} alt="Back skyline" />
+                <img className='home-bg-building-close' src={buildingClose} alt="Close skyline" />
             </section>
         </main>
     )
+
 }
 
 export default Home
