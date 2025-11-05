e9ad25006404c7df3c15acebb6dc8c1dada4862a
LiberteI <liberteix@gmail.com>
2025-11-05 12:40:24 -0400

    added nav hide feature, adjusted home

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,10 +1,33 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './Navbar.css'
 import logo from '../../assets/LOGO_dark.png'
 
 const Navbar = () => {
+  const [isHidden, setIsHidden] = useState(false)
+
+  useEffect(() => {
+    let lastScrollY = window.scrollY
+
+    const handleScroll = () => {
+      const currentY = window.scrollY
+      const scrollingDown = currentY > lastScrollY && currentY > 120
+      const scrollingUp = currentY < lastScrollY
+
+      if (scrollingDown && !isHidden) {
+        setIsHidden(true)
+      } else if (scrollingUp && isHidden) {
+        setIsHidden(false)
+      }
+
+      lastScrollY = currentY
+    }
+
+    window.addEventListener('scroll', handleScroll, { passive: true })
+    return () => window.removeEventListener('scroll', handleScroll)
+  }, [isHidden])
+
   return (
-    <nav className='navbar'>
+    <nav className={`navbar ${isHidden ? 'navbar--hidden' : ''}`}>
         <div className='navbar_logo'>
           <img src={logo} alt="" className='Logo'/>
         </div>
