385a3c9fde2d08d269b2b3a02e2ee1512fdbb937
LiberteI <liberteix@gmail.com>
2025-11-04 22:33:08 -0400

    added sky & moon scenes

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,10 +1,15 @@
 import './Home.css'
 
+import moon from '../../assets/City/bg/Moon.png'
+import sky from '../../assets/City/bg/Sky.png'
+
 const Home = () => {
     return(
         <main className='home'>
             <section className='home-scene'>
-
+                
+                <img className='home-bg-sky' src={sky} alt="sky" />
+                <img className='home-bg-moon' src={moon} alt="moon" />
             </section>
         </main>
     )
