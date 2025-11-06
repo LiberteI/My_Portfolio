ce20a6c3f90ec7d04844abae1ffbbd4056d43000
LiberteI <liberteix@gmail.com>
2025-11-06 14:33:39 -0400

    configuring yt backend, card clickable

--- frontend/src/Components/Performance/Performance.jsx
+++ frontend/src/Components/Performance/Performance.jsx
@@ -1,18 +1,21 @@
 import './Performance.css'
 const Performace = () => {
     const performances = [
-        // {
-        //     title: 'title1', 
-        //     date: 'mar 13',
-        //     description: 'this is a performace',
-        //     link: '#',
-        //     thumbnail: null,
-        // },
+        {
+            title: 'title1', 
+            date: 'mar 13',
+            description: 'this is a performace',
+            link: '#',
+            thumbnail: null,
+        },
     ]
+    const handleClick = () => {
+        console.log("hello!");
+    }
     return (
         <section className="performance"  id="performance">
             <h1>My Performances</h1>
-            <div className="performance-grid">
+            <div className="performance-grid" onClick={handleClick}>
                 {/* loop through performances array and create performance cards */}
                 { performances.map((performance) => (
                     <article className="performance-card" key={performance.title}>
