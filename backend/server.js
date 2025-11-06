5538d952c735f6b72aa0ed822e35b57fd350e854
LiberteI <liberteix@gmail.com>
2025-11-05 21:35:04 -0400

    added name guard

--- backend/server.js
+++ backend/server.js
@@ -5,13 +5,23 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const isValid = () =>{
+const isValid = ({name, email, message}) =>{
+    if (!name || !email || !message){
+        return false;
+    } 
+    if (/\d/.test(name)) {
+        return false;
+    }
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+    if(!emailRegex.test(email.trim())){
+        return false;
+    }
     return true;
 }
 app.post("/api/contact", (req, res) => {
     const { name, email, message } = req.body;
 
-    if(!isValid){
+    if(!isValid({ name, email, message })){
         return res.status(400).json({error: "Input is not valid"});
     }
 
