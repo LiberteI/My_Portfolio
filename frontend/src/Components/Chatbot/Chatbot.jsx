4b9536fe71b14bf46e5a18741bbad7f68d252444
LiberteI <liberteix@gmail.com>
2025-12-11 17:27:40 -0400

    NPC gets stable pos

--- frontend/src/Components/Chatbot/Chatbot.jsx
+++ frontend/src/Components/Chatbot/Chatbot.jsx
@@ -1,13 +1,13 @@
 import idleGIF from "../../assets/Animations/npc/Idle.gif"
 import dialogueGIF from "../../assets/Animations/npc/Dialogue.gif"
 import thumbupGIF from "../../assets/Animations/npc/ThumbUp.gif"
-
+import './Chatbot.css'
 const Chatbot = () => {
     /*
         this chatbot should be put on the roof
     */
     return(
-        <img src={idleGIF} alt="" />
+        <img className='chatbot' src={idleGIF} alt="" />
     )
 }
 
