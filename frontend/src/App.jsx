3dcb254bdd753c2df155fc5169794f85e2cc0088
LiberteI <liberteix@gmail.com>
2025-12-17 11:24:11 -0400

    fixed hidden layout

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,12 +3,13 @@ import Navbar from './Components/Navbar/Navbar'
 import Home from './Components/Home/Home'
 import Contact from './Components/Contact/Contact'
 import Performances from './Components/Performance/Performance'
-
+import About from './Components/About/About'
 const App = () => {
   return (
     <div className='app-container'>
       <Navbar/>
       <Home />
+      <About />
       <Performances/>
       <Contact />
       
---
+++ frontend/src/Components/About/About.jsx
@@ -0,0 +1,13 @@
+const About = () => {
+
+    return(
+        <section className="about-container">
+            <h1>About Me</h1>
+            <article>
+                
+            </article>
+        </section>
+    )
+}
+
+export default About
