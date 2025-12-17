7cd2928ddf60c6224b82cda68de4cdc059d9a6d2
LiberteI <liberteix@gmail.com>
2025-12-17 11:48:08 -0400

    added css for about

--- frontend/src/Components/About/About.jsx
+++ frontend/src/Components/About/About.jsx
@@ -1,11 +1,35 @@
+import './About.css'
 const About = () => {
 
     return(
         <section className="about-container">
             <h1>About Me</h1>
             <article>
-                
+                <p>My name is Yiming Yang (Liberte), a third-year Computer Science student at Dalhousie University.</p>
+
+                <p>I am a programmer with interests in game development, full-stack development, and AI & machine learning.</p>
+
+                <p>Beyond programming, I am a trained pianist, and my favorite composer is Ludwig van Beethoven.</p>
+
             </article>
+            <h2>My Tech Stack</h2>
+            <div className="stack-container">
+                <div>
+                    <h3>Languages</h3>
+                </div>
+
+                <div>
+                    <h3>Frameworks</h3>
+                </div>
+
+                <div>
+                    <h3>Libraries</h3>
+                </div>
+
+                <div>
+                    <h3>Dev Tools</h3>
+                </div>
+            </div>
         </section>
     )
 }
