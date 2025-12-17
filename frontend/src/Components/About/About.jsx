068fa003370593d66fc01a2f6e8f45c3b1eef410
LiberteI <liberteix@gmail.com>
2025-12-17 17:03:38 -0400

    added language tech stack icons

--- frontend/src/Components/About/About.jsx
+++ frontend/src/Components/About/About.jsx
@@ -1,4 +1,10 @@
 import './About.css'
+import jsIcon from '../../assets/icons/js.png'
+import tsIcon from '../../assets/icons/typescript.png'
+import pythonIcon from '../../assets/icons/python.png'
+import javaIcon from '../../assets/icons/java.png'
+import cppIcon from '../../assets/icons/cpp.png'
+import csharpIcon from '../../assets/icons/csharp.png'
 const About = () => {
 
     return(
@@ -7,7 +13,7 @@ const About = () => {
             <article className='paragraph-container'>
                 <p>  My name is Yiming Yang (Liberte), a third-year Computer Science student at Dalhousie University.</p>
 
-                <p>  I am a programmer with interests in game development, full-stack development, and AI & machine learning.</p>
+                <p>  I am a programmer with interests in game development, full-stack development, animation and AI & machine learning.</p>
 
                 <p>  Beyond programming, I am a trained pianist, and my favorite composer is Ludwig van Beethoven.</p>
 
@@ -16,14 +22,19 @@ const About = () => {
             <div className="stack-container">
                 <div>
                     <h3>Languages</h3>
+                    <div className='language-container'>
+                        <img src={csharpIcon} alt="C#" />
+                        <img src={cppIcon} alt="C++" />
+                        <img src={javaIcon} alt="Java" />
+                        <img src={jsIcon} alt="JavaScript" />
+                        <img src={pythonIcon} alt="Python" />
+                        <img src={tsIcon} alt="TypeScript" />
+                        
+                    </div>
                 </div>
 
                 <div>
                     <h3>Frameworks</h3>
-                </div>
-
-                <div>
-                    <h3>Libraries</h3>
                 </div>
 
                 <div>
