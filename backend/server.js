aab851806047e18d47050d3668d67ce20bd71424
LiberteI <liberteix@gmail.com>
2025-12-18 18:30:49 -0400

    wired up mongoDB

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,9 @@ import 'dotenv/config';
 import contactRoutes from "./contact/route.js";
 import youtubeRoutes from "./Youtube/route.js"
 
+import mongoose from "mongoose";
+
+
 const app = express();
 app.use(cors());
 app.use(express.json());
@@ -18,4 +21,14 @@ app.use("/api/contact", contactRoutes);
 app.use("/api/youtube", youtubeRoutes);
 
 const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => console.log(`backend running on port ${PORT}`));
+
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+        console.log("Mongo connected!");
+
+        app.listen(PORT, () => console.log(`backend running on port ${PORT}`));
+    })
+    .catch(err => {
+        console.error('Mongo error', err);
+        process.exit(1);
+    });
