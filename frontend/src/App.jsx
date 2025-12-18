0a2f737279d3af8770d92198f61536a6911c8171
LiberteI <liberteix@gmail.com>
2025-12-18 14:50:39 -0400

    created knight routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, useParams } from 'react-router-dom'
 import Navbar from './Components/Navbar/Navbar'
 import Home from './Components/Home/Home'
 import Contact from './Components/Contact/Contact'
 import Performances from './Components/Performance/Performance'
 import About from './Components/About/About'
 import Project from './Components/Project/Project'
+
 
 const App = () => {
   return (
@@ -34,7 +35,21 @@ const KnightGame = () => {
   return (
     <div className='app-container'>
       <Navbar/>
-      
+      <div className='knightGame-container'>
+
+        <video src="/knightTrailer.mp4" controls />
+
+        <p>
+          Gameplay trailer for <strong>Knight of Cinders</strong>.
+          Core combat systems and logic were preserved after a local
+          asset deletion incident.
+        </p>
+
+        <a href="https://drive.google.com/file/d/1QigchoK-Ckn5wDokIMy8jG526GJFgCHP/view" className="download-button">
+          Download Build for Mac
+        </a>
+        
+      </div>
     </div>
   )
 }
