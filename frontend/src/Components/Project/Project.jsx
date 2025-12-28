af9630d39d209feb3cb53c498ef49f88690e6aa9
LiberteI <liberteix@gmail.com>
2025-12-28 18:41:59 -0400

    add deadzone card

--- frontend/src/Components/Project/Project.jsx
+++ frontend/src/Components/Project/Project.jsx
@@ -1,6 +1,7 @@
 import "./Project.css"
 import ProjectCard from "./ProjectCard"
 import projectThumb from "../../assets/ProjectThumbnail/KnightThumbnail.png"
+import deadzoneThumb from "../../assets/ProjectThumbnail/deadzone_thumbnail.png"
 
 const projects = [
     {
@@ -12,7 +13,19 @@ const projects = [
         skills: "Unity · Tilemap · Cinemachine · Physics & Raycasting · Singleton Architecture · State-Driven Systems",
         description: "A dark 2D action game inspired by Souls-like combat, following a fallen knight driven by loss and revenge. The game features deliberate, stamina-based melee combat, multi-phase boss encounters with distinct attack patterns, and adaptive enemy behaviors. Atmospheric parallax-scrolled environments, and cinematic animations.",
         githubLink: "https://github.com/LiberteI/KnightOfCinders_firstProject"
+    },
+    {
+        title: "Deadzone",
+        slug: "deadzone",
+        image: deadzoneThumb,
+        canLink: true,
+        topic: "Game Development",
+        skills: "Unity · C# · Finite State Machines (FSM) · Enemy & Ally AI · Event-Driven Architecture · Gameplay Systems Design",
+        description:"Designing and developing an indie 2D post-apocalyptic zombie survival shooter focused on responsive combat, barricade defense, and intelligent AI systems. Built modular, state-driven player and enemy behaviors using clean architecture to create emergent gameplay, tactical depth, and a polished, skill-based player experience.",
+        githubLink:"https://github.com/LiberteI/DeadZone"
+
     }
+    
 
 ]
 
