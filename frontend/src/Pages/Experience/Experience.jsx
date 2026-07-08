2c6805afdc61258e5a2c6a90b85a38158bc7f7ff
LiberteI <liberteix@gmail.com>
2026-07-08 16:56:20 -0300

    remove flaky magician; repivot

--- frontend/src/Pages/Experience/Experience.jsx
+++ frontend/src/Pages/Experience/Experience.jsx
@@ -1,35 +1,11 @@
 
-const magicianImg = "/images/magician/magician.png"
-const leftArmImg = "/images/magician/left-arm.png"
-const rightArmImg = "/images/magician/right-arm.png"
+
 
 const Experience = () => {
 
     return(
         <section className="relative min-h-[720px] overflow-hidden bg-black">
-            <div className="relative z-10">
-                <h1>My Experiences</h1>
-            </div>
-
-            {/* magician background layer */}
-            <div className="pointer-events-none absolute inset-0 z-0">
-                <div className="absolute left-1/2 top-0 aspect-[1000/720] w-full max-w-[1000px] -translate-x-1/2 px-4">
-                    <img 
-                        className="absolute inset-0 h-auto w-full z-10" 
-                        src={magicianImg} alt="Magician illustration" />
-
-                    <img 
-                        className="absolute left-[-2%] top-[19%] w-[48%] z-5" 
-                        src={leftArmImg} alt="" aria-hidden="true" />
-                    <img 
-                        className="absolute left-[56%] top-[17%] w-[48%] z-5" 
-                        src={rightArmImg} alt="" aria-hidden="true" />
-                </div>
-            </div>
-
-            <div className="relative z-10">
-                {/* experience cards stage */}
-            </div>
+           
         </section>
     )
 }
