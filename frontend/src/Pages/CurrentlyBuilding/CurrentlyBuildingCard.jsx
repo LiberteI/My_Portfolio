fc5970bbf6f1b804b29ec348a0a80a8b74174a0f
LiberteI <liberteix@gmail.com>
2026-09-06 10:47:23 -0300

    populate content

--- frontend/src/Pages/CurrentlyBuilding/CurrentlyBuildingCard.jsx
+++ frontend/src/Pages/CurrentlyBuilding/CurrentlyBuildingCard.jsx
@@ -1,14 +1,13 @@
 import './CurrentlyBuilding.css';
 
-const CurrentlyBuildingCard = () => {
+const CurrentlyBuildingCard = ({ thumbnail, title, shortDescription }) => {
   return (
-    <div className="currently-building-card">
-      <img src="" alt="pixel-art-thumbnail" />
-      <h3>title</h3>
-      <p>short description</p>
-      
-    </div>
+    <article className="currently-building-card">
+      {thumbnail && <img src={thumbnail} alt={`${title} thumbnail`} loading="lazy" />}
+      <h3>{title}</h3>
+      <p>{shortDescription}</p>
+    </article>
   );
 };
 
-export default CurrentlyBuildingCard;   
+export default CurrentlyBuildingCard;
